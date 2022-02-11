c2e6831b09caab67115687c8d7dcd065fa36dd64
galron123 <99333737+galron123@users.noreply.github.com>
2022-02-11 21:43:52 +0200

    Update main.ts
    change port

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
