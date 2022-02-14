1dd47426216ec3abc82732df4f435bd8963f5603
galron123 <99333737+galron123@users.noreply.github.com>
2022-02-14 21:50:38 +0200

    Update similar.service.ts
    fix permutaion function

--- src/similar/similar.service.ts
+++ src/similar/similar.service.ts
@@ -1,66 +1,57 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { Logger } from 'nestjs-pino';
+import {HttpException, Injectable} from '@nestjs/common';
+import {Logger} from 'nestjs-pino';
+
 const fs = require('fs').promises;
 let now = require('nano-time');
+
 @Injectable()
 export class SimilarService {
-  constructor(private readonly logger: Logger) {}
+    constructor(private readonly logger: Logger) {
+    }
 }
+
 export let requestsCounter = 0;
 export let totalTime = 0;
 export let size;
+export let data;
 let obj;
 const readFromFile = async () => {
-  const dict = {};
-  const dict2 = {};
-  const data = await fs.readFile(
-    './words_clean.txt',
-    'utf8',
-  );
-  const words = data.split('\n');
-  size = words.length;
-  for (let i = 0; i < words.length; i++) {
-    const sortedWord = words[i].split('').sort();
-    if (!dict[sortedWord]) {
-      dict[sortedWord] = [];
+    const dict = {};
+    const data = await fs.readFile(
+        './words_clean.txt',
+        'utf8',
+    );
+    const words = data.split('\n');
+    size = words.length;
+    for (let i = 0; i < words.length; i++) {
+        const sortedWord = words[i].split('').sort();
+        if (!dict[sortedWord]) {
+            dict[sortedWord] = [];
+        }
+        dict[sortedWord].push(words[i]);
     }
-    dict[sortedWord].push(words[i]);
-  }
-  for (let i = 0; i < words.length; i++) {
-    const sortedWord = words[i].split('').sort();
-    dict2[words[i]] = [];
-    for (let j = 0; j < dict[sortedWord].length ;j++) {
-      if (dict[sortedWord][j] !== words[i]) {
-        dict2[words[i]].push(dict[sortedWord][j]);
-      }
-    }
-  }
-
-  return dict2;
+    return dict;
 };
 
-export let data;
 (async () => {
-  data = await readFromFile();
+    data = await readFromFile();
 })();
 
 export const permutation = async (word) => {
-  const startTime = now();
-  try {
-    obj = { similar: data[word] };
-  } catch (err) {
-    throw new HttpException(`There is no such file: ${err}`, 400);
-  }
-  const endTime = now();
-  totalTime += endTime - startTime;
-  requestsCounter++;
-  const results = JSON.stringify(obj);
-  if (!data[word]) {
-    throw new HttpException(
-      `There are no similar words for this word: ${word}`,
-      404,
-    );
-  } else {
-    return results;
-  }
+    const startTime = now();
+    const sortedWord = word.split('').sort();
+    let resArray = data[sortedWord];
+    if (resArray && resArray.length != 0) {
+        resArray.filter(item => item !== word)
+    } else {
+        throw new HttpException(
+            `There are no similar words for this word: ${word}`,
+            404,
+        );
+    }
+    obj = {similar: resArray};
+    const endTime = now();
+    totalTime += endTime - startTime;
+    requestsCounter++;
+    return JSON.stringify(obj);
 };
